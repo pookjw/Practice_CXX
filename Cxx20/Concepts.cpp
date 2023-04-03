7c7df2230b17998aa088e40177616ace0e28f604
Jinwoo Kim <kidjinwoo@me.com>
2023-04-04 00:38:46 +0900

    a

---
+++ Cxx20/Concepts.cpp
@@ -0,0 +1,12 @@
+//
+//  Concepts.cpp
+//  Cxx20
+//
+//  Created by Jinwoo Kim on 4/4/23.
+//
+
+#include "Concepts.hpp"
+
+void Concepts::foo() {
+    std::cout << std::boolalpha << std::endl;
+}
---
+++ Cxx20/Concepts.hpp
@@ -0,0 +1,18 @@
+//
+//  Concepts.hpp
+//  Cxx20
+//
+//  Created by Jinwoo Kim on 4/4/23.
+//
+
+#ifndef Concepts_hpp
+#define Concepts_hpp
+
+#include <iostream>
+
+class Concepts {
+public:
+    static void foo();
+};
+
+#endif /* Concepts_hpp */
